16754d84bf0859d27cdc5b06f6299d7c6df022c2
Roms1383 <romain.kelifa@gmail.com>
2022-08-04 19:35:27 +0700

    :bug: fix incorrect regex
    #resolve discussion_r937716034

--- utils.js
+++ utils.js
@@ -11,7 +11,8 @@ const issue = (ref) => {
 }
 
 const comment = (ref) => {
-    let found = ref.match(/#comment (.*)/i)
+    let found = ref.match(/#resolve discussion_r[0-9]{9,}(.*)/i)
+    console.info(`checking for comment:\n${found}`)
     return found ? found[1] : false
 }
 
