ddf69817fd63a0e86fe1f4dfd36ee01bbd278656
Roms1383 <romain.kelifa@gmail.com>
2022-08-05 20:19:47 +0700

    :poop: debug

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const core = require('@actions/core')
 const utils = require('./utils')
 
 const event = process.env.GITHUB_REPOSITORY
@@ -5,21 +6,24 @@ const owner = utils.owner(event)
 const repo = utils.repo(event)
 
 const analyze = async () => {
-    const { commits, ref } = await utils
-        .read(`./${process.env.GITHUB_EVENT_PATH}`)
-        .then(JSON.parse)
-    const num = utils.issue(ref)
+    // const input = core.getInput('event', { required: false })
+    const input = process.env.INPUT_EVENT
+    console.info(input)
+    // const { commits, ref } = await utils
+    //     .read(`./${process.env.GITHUB_EVENT_PATH}`)
+    //     .then(JSON.parse)
+    // const num = utils.issue(ref)
     // if (num) {
-    for (commit of commits) {
-        console.info(JSON.stringify(commit, null, 2))
-        const found = utils.comment(commit.message)
-        if (found) {
-            const comment = `${found} *from @${
-                commit.author.username
-            } in [${commit.id.substring(0, 6)}](${commit.url})*`
-            console.warn('todo')
-        }
-    }
+    // for (commit of commits) {
+    //     console.info(JSON.stringify(commit, null, 2))
+    //     const found = utils.comment(commit.message)
+    //     if (found) {
+    //         const comment = `${found} *from @${
+    //             commit.author.username
+    //         } in [${commit.id.substring(0, 6)}](${commit.url})*`
+    //         console.warn('todo')
+    //     }
+    // }
     // }
 }
 
