a84baca8be7a17b384cfcfbdbd51546ba3383833
Roms1383 <romain.kelifa@gmail.com>
2022-08-06 10:05:13 +0700

    :bug: fix instantiation

--- utils/core.js
+++ utils/core.js
@@ -1,17 +1,14 @@
-const { Octokit } = require('@octokit/request')
-const octokit = new Octokit({
-    auth: `token ${process.env.GITHUB_TOKEN}`,
+const { request } = require('@octokit/request')
+request.defaults({
+    headers: { authorization: `token ${process.env.GITHUB_TOKEN}` },
 })
 const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/')
 
 const pr = async (commit_sha) =>
-    await octokit.request(
-        'GET /repos/{owner}/{repo}/commits/{commit_sha}/pulls',
-        {
-            owner,
-            repo,
-            commit_sha,
-        }
-    )
+    await request('GET /repos/{owner}/{repo}/commits/{commit_sha}/pulls', {
+        owner,
+        repo,
+        commit_sha,
+    })
 
 module.exports = { pr }
