f9226f10fa986889d31f74d7887bb2f814b4d823
Roms1383 <romain.kelifa@gmail.com>
2022-08-06 22:40:56 +0700

    :bug: debug
    #resolves discussion_r937716034

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ const analyze = async () => {
                                         repo,
                                         num,
                                         searched,
-                                        `done in ${sha}`
+                                        `@${commit.author?.name} done in ${sha}`
                                     ).then(console.info).catch(console.error)
                                 }
                                 console.warn('TODO: pagination')
