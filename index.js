d3c36037f67cf04c81b67f78873c8819870c843e
Roms1383 <romain.kelifa@gmail.com>
2022-08-07 16:57:37 +0700

    :coffin: remove logs
    #resolves discussion_r937716034

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require('dotenv').config()
 const utils = require('./utils')
-const { box, info } = utils.log
-const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/')
+const [_, repo] = process.env.GITHUB_REPOSITORY.split('/')
 
 const analyze = async () => {
     const event = await utils.event()
     const { commits, ref } = event
     const main = event.repository.master_branch
+
     if (event.created) {
         console.info(
             'emissary does not act on a freshly created branch, skipping...'
@@ -33,16 +33,12 @@ const analyze = async () => {
         console.info('emissary does not act on your main branch, skipping...')
         return
     }
-    // info('github.event', event, true)
-    // info('ref', ref)
-    // info('commits', commits.map(({ id }) => id).join(', '))
+
     for (commit of commits) {
         const sha = commit.id
         const matches = utils.matches(commit.message)
         if (matches && commit.distinct) {
-            // box('found pattern', matches)
             const { data: prs } = await utils.core.pr(sha)
-            // info('prs', prs, true)
             for (pr of prs) {
                 if (pr.state == 'open' && !pr.locked) {
                     const { next, threads, decision, total, reviews } =
@@ -50,34 +46,13 @@ const analyze = async () => {
                             pr.base?.repo?.owner?.login,
                             pr.number
                         )
-                    // info('pr node id', pr.node_id)
-                    // info('reviews', reviews, true)
-                    // info('decision', decision)
-                    // info('total', total)
-                    // console.log('\n')
                     for (thread of threads) {
-                        // info('thread id', thread.id)
-                        // info('is thread resolved?', thread.resolved)
-                        // info('can viewer reply to thread?', thread.canReply)
-                        // info('can viewer resolve thread?', thread.canResolve)
-                        // info('path file thread', thread.path)
-                        // info('total comments', thread.total)
-                        // console.log('\n')
                         if (!thread.resolved) {
-                            // console.warn('find root comment to reply to')
                             for (comment of thread.comments) {
-                                // info(
-                                //     'commit infos:',
-                                //     `@${comment.interlocutor} said:\n${comment.message}\n(${comment.state})\npath comment file: ${comment.path}\ncomment id: ${comment.id}\ncomment url: ${comment.url}`
-                                // )
                                 const searched = comment.url
                                     .split('#')[1]
                                     .substr('discussion_r'.length)
                                 if (searched === matches) {
-                                    // info(
-                                    //     'found!',
-                                    //     `${comment.url} matches with ${matches}`
-                                    // )
                                     await utils.core.reply(
                                         pr.base?.repo?.owner?.login,
                                         repo,
