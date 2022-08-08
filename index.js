1b3eed723ec7787f3310cd694a934d6b8e0bd586
Roms1383 <romain.kelifa@gmail.com>
2022-08-08 22:21:50 +0700

    :recycle: refactor for modularity

--- index.js
+++ index.js
@@ -3,92 +3,82 @@ const { info, warning, debug, setFailed } = require('@actions/core')
 const utils = require('./utils')
 const [_, repo] = process.env.GITHUB_REPOSITORY.split('/')
 
+const matching = (kept, commit) => {
+    const sha = commit.id
+    const matches = utils.matches(commit.message)
+    if (matches && commit.distinct) kept.push({ sha, matches })
+    return kept
+}
+const opened = (pr) => pr.state == 'open' && !pr.locked
+const unresolved = (thread) => !thread.resolved
+const same = (discussion) => (comment) =>
+    comment.url.split('#')[1].substr('discussion_r'.length) === discussion
+const resolutions = ({ matches }) => matches.act === 'resolve'
+
+const handle = async ({ sha, matches }) => {
+    const { data: prs } = await utils.core.pr(sha)
+    debug(`utils.core.pr:\n${JSON.stringify(prs, null, 2)}\n\n`)
+
+    const openedPRs = (prs || []).filter(opened)
+    let source
+    outer: for (pr of openedPRs) {
+        const { previous, threads } = await utils.graphql.pr(
+            pr.base?.repo?.owner?.login,
+            pr.number
+        )
+        debug(
+            `utils.graphql.pr[threads]:\n${JSON.stringify(
+                threads,
+                null,
+                2
+            )}\n\n`
+        )
+        const unresolvedThreads = (threads || []).filter(unresolved)
+        for (thread of unresolvedThreads) {
+            source = (thread.comments || []).find(same(matches.discussion))
+            if (source) break outer
+        }
+    }
+    if (source) {
+        const act =
+            matches.act === 'reply' ? 'marked it as done' : 'resolved it'
+        let message = `@${commit.author?.name} ${act} in ${sha}`
+        if (matches.extra) message = `${message}\n${matches.extra}`
+        await utils.core.reply(
+            pr.base?.repo?.owner?.login,
+            repo,
+            pr.number,
+            searched,
+            message
+        )
+        if (matches.act === 'resolve') {
+            await utils.graphql.resolve(thread.id)
+        }
+        return true
+    }
+    return false
+}
+
 const action = async () => {
     const event = await utils.eventOrSkip()
     if (event === 'skip') process.exit(0)
 
     const { commits } = event
-    const total = {
-        commits: commits.length,
-        matches: 0,
-        replied: 0,
-        resolved: 0,
-    }
-    for (commit of commits) {
-        const sha = commit.id
-        const matches = utils.matches(commit.message)
-        if (matches && commit.distinct) {
-            total.matches++
-            const { data: prs } = await utils.core.pr(sha)
-            debug(`utils.core.pr:\n${JSON.stringify(prs, null, 2)}\n\n`)
-            for (pr of prs) {
-                if (pr.state == 'open' && !pr.locked) {
-                    const { previous, threads } = await utils.graphql.pr(
-                        pr.base?.repo?.owner?.login,
-                        pr.number
-                    )
-                    debug(
-                        `utils.graphql.pr[threads]:\n${JSON.stringify(
-                            threads,
-                            null,
-                            2
-                        )}\n\n`
-                    )
-                    for (thread of threads) {
-                        if (!thread.resolved) {
-                            for (comment of thread.comments) {
-                                const searched = comment.url
-                                    .split('#')[1]
-                                    .substr('discussion_r'.length)
-                                debug(
-                                    `comparing ${searched} with ${matches.discussion}...`
-                                )
-                                if (searched === matches.discussion) {
-                                    info(
-                                        `${matches.act} discussion ${searched} in thread ${thread.id} in PR ${pr.number}`
-                                    )
-                                    const act =
-                                        matches.act === 'reply'
-                                            ? 'marked it as done'
-                                            : 'resolved it'
-                                    let message = `@${commit.author?.name} ${act} in ${sha}`
-                                    if (matches.extra)
-                                        message = `${message}\n${matches.extra}`
-                                    await utils.core.reply(
-                                        pr.base?.repo?.owner?.login,
-                                        repo,
-                                        pr.number,
-                                        searched,
-                                        message
-                                    )
-                                    if (matches.act === 'resolve') {
-                                        await utils.graphql.resolve(thread.id)
-                                        total.resolved++
-                                    } else {
-                                        total.replied++
-                                    }
-                                }
-                                warning('TODO: pagination')
-                                if (comment.previous) {
-                                    warning('there are more comments')
-                                }
-                            }
-                        }
-                    }
-                    warning('TODO: pagination')
-                    if (previous) {
-                        warning('there are more threads')
-                    }
-                }
-            }
+    let success = []
+    const kept = commits.reduce(matching, [])
+    for (commit of kept) {
+        if (await handle(commit)) {
+            success.push(commit)
         }
     }
+    const resolved = success.filter(resolutions).length
+    const replied = success.length - resolved
     info(
-        `push event contains ${total.commits} commit(s), ${
-            total.matches
-        } match(es): ${total.replied} discussion${
-            total.replied > 1 ? 's' : ''
-        } replied to and ${total.resolved} directly resolved`
+        `push event contains ${commits.length} commit${
+            commits.length > 1 ? 's' : ''
+        }, ${kept.length} match(es): ${replied} discussion${
+            replied > 1 ? 's' : ''
+        } replied to and ${resolved} directly resolved`
     )
     info('finished')
 }
