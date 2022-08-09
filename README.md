4d6d087ce77f04c86b3662d5a9749e686d245803
Roms1383 <romain.kelifa@gmail.com>
2022-08-09 18:01:25 +0700

    :art: lint files

--- README.md
+++ README.md
@@ -36,7 +36,7 @@ jobs:
       - name: install dependencies
         run: npm ci
 
-      - uses: Roms1383/emissary@v1
+      - uses: Roms1383/emissary@v0.7
         with:
           token: ${{ secrets.GITHUB_TOKEN }} # required to act in Github on your behalf
 ```
@@ -46,38 +46,38 @@ jobs:
 1. setup the Github Action in your workflow [as shown in previous section](#setup).
 1. comment your commits like so, e.g.:
 
-    ```git
-    chore: improve documentation
+   ```git
+   chore: improve documentation
 
-    add example to the latest awesome feature.
-    resolve discussion DISCUSSION_NUMBER
-    ```
+   add example to the latest awesome feature.
+   resolve discussion DISCUSSION_NUMBER
+   ```
 
-    > :warning: `resolve` will reply to the conversation AND resolve it
-    >
-    > (which is usually what you want for trivial changes that doesn't require extra attention from the maintainer)
+   > :warning: `resolve` will reply to the conversation AND resolve it
+   >
+   > (which is usually what you want for trivial changes that doesn't require extra attention from the maintainer)
 
-    or
+   or
 
-    ```git
-    feat: add new CLI argument
+   ```git
+   feat: add new CLI argument
 
-    add an additional argument for the new feature.
-    reply discussion DISCUSSION_NUMBER
-    ```
+   add an additional argument for the new feature.
+   reply discussion DISCUSSION_NUMBER
+   ```
 
-    > :warning: `reply` will only reply to the conversation, leaving it up to the maintainer to resolve it
-    >
-    > (which is usually what you want for non-trivial changes)
+   > :warning: `reply` will only reply to the conversation, leaving it up to the maintainer to resolve it
+   >
+   > (which is usually what you want for non-trivial changes)
 
-    :arrow_right: the `DISCUSSION_NUMBER` can be found under **Copy Link** on the comment requesting changes:
+   :arrow_right: the `DISCUSSION_NUMBER` can be found under **Copy Link** on the comment requesting changes:
 
-    ![copy discussion number screenshot](images/copy-discussion-number.png)
+   ![copy discussion number screenshot](images/copy-discussion-number.png)
 
-    it should looks like, e.g. `https://github.com/Roms1383/emissary/pull/1#discussion_r937716034` where `937716034` is the discussion number: either full url or its discussion number are supported in the commit message.
+   it should looks like, e.g. `https://github.com/Roms1383/emissary/pull/1#discussion_r937716034` where `937716034` is the discussion number: either full url or its discussion number are supported in the commit message.
 
 1. on the Github Action successful completion you should now see in your PR:
 
-    ![screenshot comment](./images/comment.png)
+   ![screenshot comment](./images/comment.png)
 
 This Github Action supports a couple of different spelling, that you can check [from the unit-tests](tests/utils.test.js).
--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,78 +1,78 @@
 const utils = require('../lib/utils')
 
 describe('comment', () => {
-    describe('should return false if commit does NOT include a comment', () => {
-        it('without keyword', async () => {
-            const message = 'added initial logic'
-            const result = utils.matches(message)
-            expect(result).toBe(false)
-        })
-        it('with keyword but no discussion', async () => {
-            const message =
-                'added initial logic\n\nresolve discussion\n\nbut forgot to specify'
-            const result = utils.matches(message)
-            expect(result).toBe(false)
-        })
-        it('with keyword but malformed discussion', async () => {
-            const message =
-                'added initial logic\n\nresolve discussion 049f011f9363ddd17160c47d57380fbfe4ab8d84\n\nbut specified a commit instead'
-            const result = utils.matches(message)
-            expect(result).toBe(false)
-        })
+  describe('should return false if commit does NOT include a comment', () => {
+    it('without keyword', async () => {
+      const message = 'added initial logic'
+      const result = utils.matches(message)
+      expect(result).toBe(false)
+    })
+    it('with keyword but no discussion', async () => {
+      const message =
+        'added initial logic\n\nresolve discussion\n\nbut forgot to specify'
+      const result = utils.matches(message)
+      expect(result).toBe(false)
+    })
+    it('with keyword but malformed discussion', async () => {
+      const message =
+        'added initial logic\n\nresolve discussion 049f011f9363ddd17160c47d57380fbfe4ab8d84\n\nbut specified a commit instead'
+      const result = utils.matches(message)
+      expect(result).toBe(false)
+    })
+  })
+
+  describe('should return act, discussion and extra if commit does include a comment', () => {
+    it("with leading 'discussion'", async () => {
+      let message =
+        'added initial logic\n\nresolve discussion 937716034\n\nand some other reminder details'
+      const { act, discussion, extra } = utils.matches(message)
+      expect(act).toBe('resolve')
+      expect(discussion).toStrictEqual(['937716034'])
+      expect(extra).toBe('')
+    })
+    it("without leading 'discussion'", async () => {
+      let message =
+        'added initial logic\n\nresolve 937716034\n\nand some other reminder details'
+      const { act, discussion, extra } = utils.matches(message)
+      expect(act).toBe('resolve')
+      expect(discussion).toStrictEqual(['937716034'])
+      expect(extra).toBe('')
     })
 
-    describe('should return act, discussion and extra if commit does include a comment', () => {
-        it("with leading 'discussion'", async () => {
-            let message =
-                'added initial logic\n\nresolve discussion 937716034\n\nand some other reminder details'
-            const { act, discussion, extra } = utils.matches(message)
-            expect(act).toBe('resolve')
-            expect(discussion).toStrictEqual(['937716034'])
-            expect(extra).toBe('')
-        })
-        it("without leading 'discussion'", async () => {
-            let message =
-                'added initial logic\n\nresolve 937716034\n\nand some other reminder details'
-            const { act, discussion, extra } = utils.matches(message)
-            expect(act).toBe('resolve')
-            expect(discussion).toStrictEqual(['937716034'])
-            expect(extra).toBe('')
-        })
-
-        it('without extra', async () => {
-            let message =
-                'added initial logic\n\nresolve 937716034\n\nand some other reminder details'
-            const { act, discussion, extra } = utils.matches(message)
-            expect(act).toBe('resolve')
-            expect(discussion).toStrictEqual(['937716034'])
-            expect(extra).toBe('')
-        })
-
-        it('with extra', async () => {
-            let message =
-                'added initial logic\n\nresolve 937716034 also thanks for your review\n\nand some other reminder details'
-            const { act, discussion, extra } = utils.matches(message)
-            expect(act).toBe('resolve')
-            expect(discussion).toStrictEqual(['937716034'])
-            expect(extra).toBe('also thanks for your review')
-        })
-
-        it('with multiple discussions', async () => {
-            let message =
-                'added initial logic\n\nresolve 937716034 940389955\n\nand some other reminder details'
-            const { act, discussion, extra } = utils.matches(message)
-            expect(act).toBe('resolve')
-            expect(discussion).toStrictEqual(['937716034', '940389955'])
-            expect(extra).toBe('')
-        })
-
-        it('with urls', async () => {
-            let message =
-                'added initial logic\n\nresolve https://github.com/Roms1383/emissary/pull/10#discussion_r937716034 https://github.com/Roms1383/emissary/pull/10#discussion_r940389955\n\nand some other reminder details'
-            const { act, discussion, extra } = utils.matches(message)
-            expect(act).toBe('resolve')
-            expect(discussion).toStrictEqual(['937716034', '940389955'])
-            expect(extra).toBe('')
-        })
+    it('without extra', async () => {
+      let message =
+        'added initial logic\n\nresolve 937716034\n\nand some other reminder details'
+      const { act, discussion, extra } = utils.matches(message)
+      expect(act).toBe('resolve')
+      expect(discussion).toStrictEqual(['937716034'])
+      expect(extra).toBe('')
     })
+
+    it('with extra', async () => {
+      let message =
+        'added initial logic\n\nresolve 937716034 also thanks for your review\n\nand some other reminder details'
+      const { act, discussion, extra } = utils.matches(message)
+      expect(act).toBe('resolve')
+      expect(discussion).toStrictEqual(['937716034'])
+      expect(extra).toBe('also thanks for your review')
+    })
+
+    it('with multiple discussions', async () => {
+      let message =
+        'added initial logic\n\nresolve 937716034 940389955\n\nand some other reminder details'
+      const { act, discussion, extra } = utils.matches(message)
+      expect(act).toBe('resolve')
+      expect(discussion).toStrictEqual(['937716034', '940389955'])
+      expect(extra).toBe('')
+    })
+
+    it('with urls', async () => {
+      let message =
+        'added initial logic\n\nresolve https://github.com/Roms1383/emissary/pull/10#discussion_r937716034 https://github.com/Roms1383/emissary/pull/10#discussion_r940389955\n\nand some other reminder details'
+      const { act, discussion, extra } = utils.matches(message)
+      expect(act).toBe('resolve')
+      expect(discussion).toStrictEqual(['937716034', '940389955'])
+      expect(extra).toBe('')
+    })
+  })
 })
